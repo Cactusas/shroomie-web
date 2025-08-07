e0c113745755b1ba2ff155f36c765241db3b48c4
Tadas <tadas@elseta.com>
2025-08-07 12:58:07 +0300

    Add compression to all files and naming without hashes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import compression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression({
+      filter: /\.(js|mjs|json|css|html|svg)$/i,
+      threshold: 1,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Remove content hash from JS and CSS
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
 })
